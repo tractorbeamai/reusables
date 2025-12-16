2794e3202ea86e4d4c844b8b8cedb8ab77d01317
jkunkel-tractorbeam <justin@tractorbeam.ai>
2025-12-16 15:22:59 -0600

    fix(eslint-config): disable `unicorn/no-array-sort` (#6)

--- packages/eslint-config/src/typescript.ts
+++ packages/eslint-config/src/typescript.ts
@@ -39,6 +39,7 @@ export const typescript: Linter.Config[] = defineConfig([
       "unicorn/no-null": "off",
       "unicorn/prevent-abbreviations": "off",
       "unicorn/no-abusive-eslint-disable": "off",
+      "unicorn/no-array-sort": "off",
     },
   },
 ]);

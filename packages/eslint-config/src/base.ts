0d267029c0c8c41081c5ab9d189f48c402f5f077
wadefletch <wbfletch@gmail.com>
2025-12-18 01:04:55 -0600

    feat(prettier-plugin): add eslint-plugin-depend

--- packages/eslint-config/src/base.ts
+++ packages/eslint-config/src/base.ts
@@ -1,9 +1,11 @@
 import eslint from "@eslint/js";
 import type { Linter } from "eslint";
+import { configs as pluginDependConfigs } from "eslint-plugin-depend";
 import { defineConfig } from "eslint/config";
 
 export const base: Linter.Config[] = defineConfig([
   eslint.configs.recommended,
+  pluginDependConfigs["flat/recommended"],
   {
     rules: {
       "no-nested-ternary": "error",
